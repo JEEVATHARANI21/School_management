fa62c192d4deb12d1a841ca99ce1a697590e2507
S.jeevatharani <jeevatharani2172005@gmail.com>
2025-09-18 07:03:26 +0000

    com

--- schoolmanagement/core/models.py
+++ schoolmanagement/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone 
+from django.utils import timezone
 
 class StudentRegistration(models.Model):
     first_name = models.CharField(max_length=100)
